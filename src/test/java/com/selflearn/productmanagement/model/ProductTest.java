00c0b205e585be5cf252d39e023f91752b609aaa
girishu-ms26 <108251671+girishu-ms26@users.noreply.github.com>
2022-06-27 19:19:00 +0530

    ProductID Test

--- src/test/java/com/selflearn/productmanagement/model/ProductTest.java
+++ src/test/java/com/selflearn/productmanagement/model/ProductTest.java
@@ -11,6 +11,29 @@ public class ProductTest {
 
     @Test
     public void productIdTest() {
+        product.setId("1");
+        assertThat(product.getId()).isEqualTo("1");
         assertThat(p1.getId()).isEqualTo("1");
+    }
+
+    @Test
+    public void productPriceTest(){
+        product.setPrice(123);
+        assertThat(product.getPrice()).isEqualTo(123);
+        assertThat(p1.getPrice()).isEqualTo(20);
+    }
+
+    @Test
+    public void productAvailableQtyTest(){
+        product.setAvailable_quantity(123);
+        assertThat(product.getAvailable_quantity()).isEqualTo(123);
+        assertThat(p1.getAvailable_quantity()).isEqualTo(1);
+    }
+
+    @Test
+    public void product_nameTest(){
+        product.setProduct_name("Iphone");
+        assertThat(product.getProduct_name()).isEqualTo("Iphone");
+        assertThat(p1.getProduct_name()).isEqualTo("ABCD");
     }
 }

b07644205109201eaf9932a74d78b18500b4e01a
girishu <girishums1999@gmail.com>
2022-06-27 11:53:13 +0530

    Added Get Post Put and Delete end points

--- src/main/java/com/selflearn/productmanagement/controller/ProductManagementController.java
+++ src/main/java/com/selflearn/productmanagement/controller/ProductManagementController.java
@@ -1,10 +1,41 @@
 package com.selflearn.productmanagement.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.selflearn.productmanagement.model.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductManagementController {
+
+    @GetMapping("/products")
+    public List<Product> getAllProductsList(){
+        return null;
+    }
+
+    @GetMapping("/products/{id}")
+    public Product getProductById(@PathVariable String id){
+        return null;
+    }
+
+    @PostMapping("/product")
+    public String saveProduct(@RequestBody Product product){
+        return "Saved Product with name "+product.getProduct_name();
+    }
+
+    @PostMapping("/products")
+    public String saveListOfProducts(@RequestBody List<Product> products){
+        return "Saved "+products.size()+ " Products";
+    }
+
+    @PutMapping("/products")
+    public String updateProduct(@RequestBody Product product){
+        return "Updated product with id "+product.getId();
+    }
+
+    @DeleteMapping("/products/{id}")
+    public String deleteProduct(@PathVariable String id){
+        return "Deleted product with ID "+id;
+    }
 }

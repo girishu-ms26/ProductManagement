5c8cf51008a6202498c6f62c3bb19b36ff0759d5
girishu <girishums1999@gmail.com>
2022-06-27 11:30:21 +0530

    Made changes to Product Model class

--- src/main/java/com/selflearn/productmanagement/model/Product.java
+++ src/main/java/com/selflearn/productmanagement/model/Product.java
@@ -1,4 +1,20 @@
 package com.selflearn.productmanagement.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Product {
+
+    @Id
+    private String id;
+    private String product_name;
+    private String available_quantity;
+    private String price;
 }

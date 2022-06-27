a2479a3717edeb89795010c5959a07038c8690dc
girishu <girishums1999@gmail.com>
2022-06-27 10:59:28 +0530

    Added @RestController annotation

--- src/main/java/com/selflearn/productmanagement/controller/ProductManagementController.java
+++ src/main/java/com/selflearn/productmanagement/controller/ProductManagementController.java
@@ -1,4 +1,7 @@
 package com.selflearn.productmanagement.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductManagementController {
 }

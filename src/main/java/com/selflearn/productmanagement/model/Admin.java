70ad4c91339ec4763f88f42505168bea433a97e7
girishu-ms26 <108251671+girishu-ms26@users.noreply.github.com>
2022-06-28 16:15:33 +0530

    Initialized Admin model class

--- src/main/java/com/selflearn/productmanagement/model/Admin.java
+++ src/main/java/com/selflearn/productmanagement/model/Admin.java
@@ -1,4 +1,21 @@
 package com.selflearn.productmanagement.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "admin")
 public class Admin {
+
+    @Id
+    private String Id;
+    private String admin_email;
+    private String admin_password;
 }
